dbcdcd1d8a49ff56b5432ee3d7caf49e3718509b
Holayn <kwwong1597@gmail.com>
2026-02-20 18:05:55 -0500

    Update CSP.

--- packages/server/src/middleware/security.ts
+++ packages/server/src/middleware/security.ts
@@ -12,11 +12,18 @@ export const nonceGenerator = (
 
 export const cspDirectives = {
   defaultSrc: ["'self'"],
+  // data: allows Shoelace SVG icons
+  connectSrc: ["'self'", 'data:'],
   // data: allows VideoJS base64-encoded WOFF fonts
   fontSrc: ["'self'", 'data:'],
   imgSrc: ['*'],
   objectSrc: ["'none'"],
-  scriptSrc: ["'self'", 'https://cdn.jsdelivr.net'],
+  scriptSrc: [
+    "'self'",
+    'https://cdn.jsdelivr.net',
+    // Hash of the inline theme-detection script in index.html
+    "'sha256-iFehDCUjWEzsexR7um+Z0ehvRw9ACAS+OjmpeCbwHO0='",
+  ],
   styleSrc: ["'self'", 'https://cdn.jsdelivr.net'],
   // blob: allows VideoJS blob workers
   workerSrc: ["'self'", 'blob:'],
